087a5fb0f34fe06b9e60d41d0eccc98cbf283ebb
Vijay2K1 <vas072001@gmail.com>
2025-03-09 22:18:30 +0530

    Initial commit

---
+++ Day8/MVC-EFCore/MVC-EFCore/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_EFCore.Models;
+
+namespace MVC_EFCore.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)  : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=VIJZZZ\\MSSQLSERVER01;database=DemoDb2;integrated security=true;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ Day8/MVC-EFCore/MVC-EFCore/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_EFCore.Models
+{
+    public class Employee
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public string Dept { get; set; }
+        public int Salary { get; set; }
+    }
+
+}
